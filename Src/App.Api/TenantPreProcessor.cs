4c46b590fb39e6d517e9d69e8973935258bfa86d
gurolg <gurol@gurkan.org>
2023-12-01 15:35:45 +0300

    Update package references to latest versions

--- Src/App.Api/TenantPreProcessor.cs
+++ Src/App.Api/TenantPreProcessor.cs
@@ -3,36 +3,29 @@ using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
 
 namespace Yaver.App;
-
 /// <summary>
-/// Represents a pre-processor that validates the tenant ID header in the HTTP request.
+/// Represents a pre-processor that handles tenant-specific operations before processing the request.
 /// </summary>
-public class TenantPreProcessor : IGlobalPreProcessor {
+public class TenantPreProcessor : IGlobalPreProcessor
+{
   /// <summary>
-  /// Pre-processes the incoming request by checking the X-tenant-id header and validating its value.
-  /// If the header is missing or the value is not valid, it sends an appropriate response.
+  /// Represents an asynchronous operation that performs pre-processing tasks.
   /// </summary>
-  /// <param name="req">The incoming request object.</param>
-  /// <param name="ctx">The current HTTP context.</param>
-  /// <param name="failures">A list of validation failures to add to if the header is missing or the value is not valid.</param>
-  /// <param name="ct">A cancellation token to observe while waiting for the task to complete.</param>
+  /// <param name="ctx">The pre-processor context.</param>
+  /// <param name="ct">The cancellation token.</param>
   /// <returns>A task that represents the asynchronous operation.</returns>
-  public Task PreProcessAsync(
-    object req,
-    HttpContext ctx,
-    List<ValidationFailure> failures,
-    CancellationToken ct) {
+  public Task PreProcessAsync(IPreProcessorContext ctx, CancellationToken ct)
+  {
+    var tenantId = ctx.HttpContext.Request.Headers["X-tenant-id"].FirstOrDefault();
 
-    var tenantID = ctx.Request.Headers["X-tenant-id"].FirstOrDefault();
-    // ctx.Features.Set("tenant-id", tenantID);
-
-    if (tenantID == null) {
-      failures.Add(new("MissingHeaders", "The [x-tenant-id] header needs to be set!"));
-      return ctx.Response.SendErrorsAsync(failures, cancellation: ct); //sending response here
+    if (tenantId == null)
+    {
+      ctx.ValidationFailures.Add(new("MissingHeaders", "The [x-tenant-id] header needs to be set!"));
+      return ctx.HttpContext.Response.SendErrorsAsync(ctx.ValidationFailures, cancellation: ct);
     }
 
-    if (tenantID != "qwerty")
-      return ctx.Response.SendForbiddenAsync(cancellation: ct); //sending response here
+    if (tenantId != "qwerty")
+      return ctx.HttpContext.Response.SendForbiddenAsync(cancellation: ct);
 
     return Task.CompletedTask;
   }

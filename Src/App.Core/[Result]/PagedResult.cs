382afe320aa0b289d21458d9d94ee40735b76252
gurolg <gurol@gurkan.org>
2023-12-01 22:31:23 +0300

    Update PagedResult class to include TotalCount property

--- Src/App.Core/[Result]/PagedResult.cs
+++ Src/App.Core/[Result]/PagedResult.cs
@@ -3,9 +3,12 @@
 namespace Yaver.App;
 
 /// <summary>
-/// Represents a paged result that contains the total count and the value.
+/// Represents a paged result containing a list of items and the total count.
 /// </summary>
-/// <typeparam name="T">The type of the value.</typeparam>
+/// <typeparam name="T">The type of items in the paged result.</typeparam>
 public class PagedResult<T>(int totalCount, List<T> items) : Result<List<T>>(items) {
+  /// <summary>
+  /// Gets the total count of items.
+  /// </summary>
   public int TotalCount { get; } = totalCount;
 }

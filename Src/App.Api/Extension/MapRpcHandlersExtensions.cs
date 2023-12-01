9a02849628b8f82b2b464c8ae85d6b5e502e644d
gurolg <gurol@gurkan.org>
2023-12-01 16:16:33 +0300

    Refactor MapRpcHandlersExtensions.cs to map RPC handlers to a host

--- Src/App.Api/Extension/MapRpcHandlersExtensions.cs
+++ Src/App.Api/Extension/MapRpcHandlersExtensions.cs
@@ -1,84 +1,35 @@
-using System.Reflection;
-
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 
 namespace Yaver.App;
 
+
 /// <summary>
-/// Provides extension methods for mapping RPC handlers to a web application.
+/// Provides extension methods for mapping RPC handlers to a host.
 /// </summary>
 public static class MapRpcHandlersExtensions {
   /// <summary>
-  /// Represents a minimal API web application.
+  /// Maps RPC handlers to the specified host and service URL.
   /// </summary>
-  public static WebApplication MapRpcHandlers(
-    this WebApplication app,
-    string serviceName,
-    string serviceUrl
-  ) {
-    if (serviceUrl is null) throw new ArgumentNullException(nameof(serviceUrl));
-
-    if (serviceName is null) throw new ArgumentNullException(nameof(serviceName));
-
-    var commands = AppDomain.CurrentDomain
-      .GetAssemblies().First(x => x.GetName().Name == serviceName)
-      .GetTypes()
-      .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRpcCommand<>)))
-      .Where(t => !t.IsInterface && !t.IsAbstract);
-
-    app.MapRemote(serviceUrl, rc => {
-      var registerMethod = rc
-        .GetType()
-        .GetMethods()
-        //TODO: fix this
-        .Last(m => m.Name == "Register");
-
-      rc.ChannelOptions.MaxRetryAttempts = 3;
-
-      foreach (var cmd in commands) {
-        var args = cmd
-          .GetInterface("IRpcCommand`1")?
-          .GetGenericArguments()!;
-
-        registerMethod
-          .MakeGenericMethod([cmd, args[0]])
-          .Invoke(rc, []);
-      }
-    });
-
-    return app;
-  }
-
+  /// <param name="host">The host to map the RPC handlers to.</param>
+  /// <param name="serviceName">The name of the service assembly.</param>
+  /// <param name="serviceUrl">The URL of the service.</param>
+  /// <returns>The updated host with the mapped RPC handlers.</returns>
   public static IHost MapRpcHandlers(
     this IHost host,
-    string? serviceName,
-    string? serviceUrl
+    string serviceName,
+    string serviceUrl
   ) {
     ArgumentNullException.ThrowIfNull(serviceUrl);
 
     ArgumentNullException.ThrowIfNull(serviceName);
 
-    var fepisAssembly = Assembly.Load("fepis");
-
-    ArgumentNullException.ThrowIfNull(fepisAssembly);
-
-    var adminServiceBaseAssemblyName = fepisAssembly
-      .GetReferencedAssemblies()
-      .SingleOrDefault(an => an.Name == serviceName);
-
-    ArgumentNullException.ThrowIfNull(adminServiceBaseAssemblyName);
-
-    var adminServiceBaseAssembly = Assembly.Load(adminServiceBaseAssemblyName);
-
-    ArgumentNullException.ThrowIfNull(adminServiceBaseAssembly);
-
-    var commands = adminServiceBaseAssembly
+    var commands = AppDomain.CurrentDomain
+      .GetAssemblies().First(predicate: x => x.GetName().Name == serviceName)
       .GetTypes()
       .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRpcCommand<>)))
-      .Where(t => t is { IsInterface: false, IsAbstract: false });
+      .Where(t => !t.IsInterface && !t.IsAbstract);
 
-    host.MapRemote(serviceUrl, rc => {
+    host.MapRemote(remoteAddress: serviceUrl, r: rc => {
       var registerMethod = rc
         .GetType()
         .GetMethods()

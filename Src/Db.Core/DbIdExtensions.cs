fdf0326c2a0603281b99f655b6dcb6904aa1be04
modabas <71963375+modabas@users.noreply.github.com>
2024-07-09 10:47:55 +0300

    - update uuidnext package to latest version - update DbIdExtensions to not allocate memory for byte array during operation

--- Src/Db.Core/DbIdExtensions.cs
+++ Src/Db.Core/DbIdExtensions.cs
@@ -5,24 +5,14 @@ public static class DbIdExtensions {
   private const long UnixEpochMilliseconds = 62_135_596_800_000;
   private const long TicksPerMillisecond = 10_000;
 
-  private static byte[] GetBytes(this DbId dbId) {
-    var bytes = dbId.ToGuid().ToByteArray();
-    if (BitConverter.IsLittleEndian) {  // little endian pretends the first 8 bytes are int, short, short
-      (bytes[0], bytes[1], bytes[2], bytes[3]) = (bytes[3], bytes[2], bytes[1], bytes[0]);
-      (bytes[4], bytes[5]) = (bytes[5], bytes[4]);
-      (bytes[6], bytes[7]) = (bytes[7], bytes[6]);
-    }
-    return bytes;
+  private static bool IsV7Internal(Span<byte> bytes) {
+    return (bytes[6] & 0xF0) == 0x70;
   }
 
-  private static (bool, byte[]) IsUuidV7Internal(this DbId dbId) {
-    var bytes = dbId.GetBytes();
-    return ((bytes[6] & 0xF0) == 0x70, bytes);
-  }
-
-  public static bool IsUuidV7(this DbId dbId) {
-    var (isV7, _) = dbId.IsUuidV7Internal();
-    return isV7;
+  public static bool IsV7(this DbId dbId) {
+    Span<byte> bytes = stackalloc byte[16];
+    dbId.ToGuid().TryWriteBytes(bytes, bigEndian: true, out var _);
+    return IsV7Internal(bytes);
   }
 
   public static DateTime GetDateTime(this DbId dbId) {
@@ -34,7 +24,9 @@ public static class DbIdExtensions {
 
   public static bool TryGetDateTime(this DbId dbId, [NotNullWhen(true)] out DateTime? dateTime) {
     dateTime = null;
-    var (isV7, bytes) = dbId.IsUuidV7Internal();
+    Span<byte> bytes = stackalloc byte[16];
+    dbId.ToGuid().TryWriteBytes(bytes, bigEndian: true, out var _);
+    var isV7 = IsV7Internal(bytes);
     if (!isV7) { return false; }
     var unixMs = (long)bytes[0] << 40 | (long)bytes[1] << 32 | (long)bytes[2] << 24 | (long)bytes[3] << 16 | (long)bytes[4] << 8 | bytes[5];
     var ticks = (UnixEpochMilliseconds + unixMs) * TicksPerMillisecond;
@@ -51,7 +43,9 @@ public static class DbIdExtensions {
 
   public static bool TryGetDateTimeOffset(this DbId dbId, [NotNullWhen(true)] out DateTimeOffset? dateTimeOffset) {
     dateTimeOffset = null;
-    var (isV7, bytes) = dbId.IsUuidV7Internal();
+    Span<byte> bytes = stackalloc byte[16];
+    dbId.ToGuid().TryWriteBytes(bytes, bigEndian: true, out var _);
+    var isV7 = IsV7Internal(bytes);
     if (!isV7) { return false; }
     var unixMs = (long)bytes[0] << 40 | (long)bytes[1] << 32 | (long)bytes[2] << 24 | (long)bytes[3] << 16 | (long)bytes[4] << 8 | bytes[5];
     var ticks = (UnixEpochMilliseconds + unixMs) * TicksPerMillisecond;

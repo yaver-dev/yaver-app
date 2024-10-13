76b7b24c6128389c8dc159c75d3563ba34789813
SrkN <agdelens@gmail.com>
2024-10-13 18:28:22 +0300

    default constructor added to PgDbContext with this we can configure connection on onConfiguring override not strictly on constructor.
    linq helper class has been improved, and it accepts int, bool and enum types as term without breaking and changing anything.
    
    also I have fixed sample project and in memory sample db context

--- Sample/Services/Admin.Service/Data/ServiceDbContext.cs
+++ Sample/Services/Admin.Service/Data/ServiceDbContext.cs
@@ -3,7 +3,6 @@ using Admin.Service.Features.Tenants.Entities;
 
 using Microsoft.EntityFrameworkCore;
 
-using Yaver.App;
 using Yaver.Db;
 
 namespace Admin.Service.Data;
@@ -11,37 +10,16 @@ namespace Admin.Service.Data;
 public class ServiceDbContext : InMemoryDbContext {
   private const string Schema = "multitenancy";
 
-  public ServiceDbContext(IConfiguration configuration, IAuditMetadata auditMetadata)
-    : base(auditMetadata.AuditInfo.UserId) {
-    var connectionString = configuration.GetSection("ConnectionString").Value;
-  }
 
-
-  public DbSet<DatabaseServer> DatabaseServers { get; set; }
-  public DbSet<Tenant> Tenants { get; set; }
+  public DbSet<DatabaseServer> DatabaseServers { get; init; }
+  public DbSet<Tenant> Tenants { get; init; }
 
 
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
     optionsBuilder.UseInMemoryDatabase(Schema);
-    // base.OnConfiguring(optionsBuilder);
-    // var connStr = _configuration.GetSection("MainDbConnectionString").Value;
-
-    // _logger.LogInformation($"mainDbConn: {connStr}");
-    // if (!optionsBuilder.IsConfigured) {
-    // 	optionsBuilder.UseNpgsql(
-    // 			connStr,
-    // 			b => {
-    // 				b.MigrationsAssembly("Admin.Service.App");
-    // 				b.MigrationsHistoryTable("__EFMigrationsHistory", Schema);
-    // 			}
-    // 	);
-    // }
   }
 
   protected override void OnModelCreating(ModelBuilder builder) {
-    // base.OnModelCreating(builder);
-
-    // builder.HasDefaultSchema(Schema);
     builder.ApplyConfiguration(new DatabaseServerEntityTypeConfiguration());
   }
 }
--- Src/Db.InMemory/InMemoryDbContext.cs
+++ Src/Db.InMemory/InMemoryDbContext.cs
@@ -3,7 +3,23 @@
 namespace Yaver.Db;
 
 /// <summary>
-///   Represents an in-memory database context for the current user.
+///   Represents an in-memory database context for temporary and testing purposes.
 /// </summary>
-public class InMemoryDbContext(DbContextOptions options) : DbContext(options) {
+/// <remarks>
+///   This context is intended for use with in-memory data storage, typically in scenarios such as
+///   unit testing or temporary data management, without the need for a persistent database.
+/// </remarks>
+public class InMemoryDbContext : DbContext {
+  /// <summary>
+  ///   Initializes a new instance of the <see cref="InMemoryDbContext"/> class with default options.
+  /// </summary>
+  protected InMemoryDbContext() : base(new DbContextOptions<InMemoryDbContext>()) {
+  }
+
+  /// <summary>
+  ///   Initializes a new instance of the <see cref="InMemoryDbContext"/> class with the specified options.
+  /// </summary>
+  /// <param name="options">The options to configure the context.</param>
+  public InMemoryDbContext(DbContextOptions options) : base(options) {
+  }
 }

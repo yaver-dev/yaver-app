967176b8988d0a2a0381f3c20d77a4b5247ebc3a
gurolg <gurol@gurkan.org>
2025-04-15 18:09:48 +0300

    Remove Serilog package references and related code to streamline project dependencies

--- Src/App.Core/[Extension]/WebApplicationBuilderExtensions.cs
+++ Src/App.Core/[Extension]/WebApplicationBuilderExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 
-using Serilog;
-
 // ReSharper disable once CheckNamespace
 namespace Yaver.App;
 
@@ -10,32 +8,6 @@ namespace Yaver.App;
 ///   Provides extension methods for configuratıon
 /// </summary>
 public static class WebApplicationBuilderExtensions {
-  /// <summary>
-  ///   Provides a way to configure a <see cref="Microsoft.AspNetCore.Builder.WebApplication" />.
-  /// </summary>
-  public static WebApplicationBuilder AddYaverLogger(this WebApplicationBuilder builder) {
-    _ = builder.Host.UseSerilog((ctx, lc) => lc
-      .ReadFrom.Configuration(ctx.Configuration)
-      .Enrich.WithProperty("AppName", Environment.GetEnvironmentVariable("YAVER_APP_NAME") ?? "YAVER_APP")
-    );
-
-    return builder;
-  }
-  
-  // public static WebApplicationBuilder AddYaverLogger(
-  //   this WebApplicationBuilder builder,
-  //   Action<HostBuilderContext, LoggerConfiguration> auditLogConfiguraton) {
-
-  //   builder.Host.UseSerilog((ctx, lc) => {
-  //     lc.ReadFrom.Configuration(ctx.Configuration)
-  //     .Enrich.WithProperty("AppName", Environment.GetEnvironmentVariable("YAVER_APP_NAME") ?? "YAVER_APP");
-  //     if (auditLogConfiguraton is not null) {
-  //       lc.Destructure.With<LogWrapperDestructuringPolicy>();
-  //       auditLogConfiguraton(ctx, lc);
-  //     }
-  //   });
-  //   return builder;
-  // }
 
   /// <summary>
   ///   Provides a way to add configuration jsons
@@ -49,19 +21,3 @@ public static class WebApplicationBuilderExtensions {
     return builder;
   }
 }
-
-// public class LogWrapperDestructuringPolicy : IDestructuringPolicy {
-//   public bool TryDestructure(
-//     object value,
-//     ILogEventPropertyValueFactory propertyValueFactory,
-//     [NotNullWhen(true)] out LogEventPropertyValue? result) {
-
-//     if (value is ILogWrapper l) {
-//       // Get underlying object and stop destructuring
-//       result = propertyValueFactory.CreatePropertyValue(l.Wrapped, false);
-//       return true;
-//     }
-//     result = null;
-//     return false; // Let the default policy handle it
-//   }
-// }
